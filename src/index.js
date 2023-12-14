4594cac9e6aaa1dadae424ab9a1ea9ca947bf4fa
Gulistan Boylu <gulistan.boylu@gmail.com>
2023-12-14 17:33:49 +0000

    Create user object and send post request to create user

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const axios = require('axios')
-
+const User = require('./user')
 console.log("Hi coyote, let's have some JavaScript fun! Yey!")
 
 //fetch the users with axios
 axios.get('http://localhost:3000/users').then(response => {
   console.log(response.data)
 })
+
+// create a user
+async function main() {
+  const response = await axios
+    .post('http://localhost:3000/users', {
+      name: 'Rose',
+    })
+    .then(response => {
+      console.log(response.data)
+    })
+}
+
+main()
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,22 +1,15 @@
 var express = require('express')
 var router = express.Router()
-
+const User = require('../user')
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.status(200).send({
-    user: { name: 'Defne' },
-    users: [
-      {
-        name: 'Defne',
-      },
-      {
-        name: 'Gulistan',
-      },
-      {
-        name: 'Muharrem',
-      },
-    ],
-  })
+  res.status(200).send([{ name: 'Defne' }, { name: 'Mama' }, { name: 'Baba' }])
+})
+
+// Create a new user
+router.post('/', function (req, res, next) {
+  const user = new User(req.body.name)
+  res.send(user)
 })
 
 module.exports = router
