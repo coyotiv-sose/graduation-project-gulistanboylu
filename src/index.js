cf01ee28a5100e8ea8728c0048a335186176e529
Gulistan Boylu <gulistan.boylu@gmail.com>
2023-12-14 06:33:00 +0000

    Fetch the list of user to see user list on the console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+const axios = require('axios')
+
 console.log("Hi coyote, let's have some JavaScript fun! Yey!")
+
+//fetch the users with axios
+axios.get('http://localhost:3000/users').then(response => {
+  console.log(response.data)
+})
