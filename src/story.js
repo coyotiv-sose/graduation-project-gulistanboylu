20767f8325ea1f0d7fbb7e8aebf8784855d01cb3
Gulistan Boylu <gulistan.boylu@gmail.com>
2023-12-18 18:49:02 +0000

    Create story object then user can add story

---
+++ src/story.js
@@ -0,0 +1,11 @@
+class Story {
+  constructor(hero, title, topic, favoruitePeople, heroDescription) {
+    this.hero = hero
+    this.title = title
+    this.topic = topic
+    this.favoruitePeople = favoruitePeople
+    this.heroDescription = heroDescription
+  }
+}
+
+module.exports = Story
