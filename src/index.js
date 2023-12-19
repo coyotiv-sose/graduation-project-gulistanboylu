620ef244cef236b139bacb5c8231bc8a25938ed4
Gulistan Boylu <gulistan.boylu@gmail.com>
2023-12-19 11:05:51 +0000

    Introduce createStory function for sending requests for code maintainability and reusability

--- src/index.js
+++ src/index.js
@@ -17,6 +17,21 @@ async function createUser(name, childAge, email) {
   })
 }
 
+async function createStory(email, hero, title, topic, favoruitePeople, heroDescription) {
+  const story = await axios
+    .post(`${userEndPoint}/1`, {
+      email,
+      hero,
+      title,
+      topic,
+      favoruitePeople,
+      heroDescription,
+    })
+    .then(res => {
+      console.log('response data ', res.data)
+    })
+}
+
 //create a user
 async function main() {
   createUser('Rose', 2, 'asd@gmail.com')
@@ -25,18 +40,23 @@ async function main() {
   const allUsers = await axios.get(userEndPoint)
   console.log(allUsers.data)
 
-  const roseFirstStory = await axios
-    .post(`${userEndPoint}/1`, {
-      email: 'asd@gmail.com',
-      hero: 'Zoe',
-      title: 'Bed Time for Zeo',
-      topic: 'Bed routine for 2 years old girl, she is trying to adapt with her bedtime routine',
-      favoruitePeople: 'Mum, Dad',
-      heroDescription: '2 years old girl with currly brown hair, lovely simile',
-    })
-    .then(res => {
-      console.log('response data ', res.data)
-    })
+  createStory(
+    'asd@gmail.com',
+    'Zoe',
+    'Bed Time for Zeo',
+    'Bed routine for 2 years old girl, she is trying to adapt with her bedtime routine',
+    'Mum',
+    '2 years old girl with currly brown hair, lovely simile'
+  )
+
+  createStory(
+    'hdede@gmail.com',
+    'Bea',
+    'Bea gets a checkup',
+    'Doctor checkoup for 2 year old and she will have her vaccination',
+    'Mum, Dad',
+    '2 years old girl with strait blonde hair, lovely simile with a blue eyes'
+  )
 }
 
 main()
