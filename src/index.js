3b0bdb0b59c6e220a111269fba0ee8f88cb1059e
Gulistan Boylu <gulistan.boylu@gmail.com>
2023-12-19 08:39:06 +0000

    Create a variable for more maintainable user class

--- src/index.js
+++ src/index.js
@@ -9,23 +9,24 @@ console.log("Hi coyote, let's have some JavaScript fun! Yey!")
 
 //create a user
 async function main() {
-  const rose = await axios.post('http://localhost:3000/users', {
+  const userEndPoint = 'http://localhost:3000/users'
+  const rose = await axios.post(userEndPoint, {
     name: 'Rose',
     childAge: 2,
     email: 'asd@gmail.com',
   })
 
-  const buket = await axios.post('http://localhost:3000/users', {
+  const buket = await axios.post(userEndPoint, {
     name: 'Buket',
     childAge: 1,
     email: 'hdede@gmail.com',
   })
 
-  const allUsers = await axios.get('http://localhost:3000/users')
+  const allUsers = await axios.get(userEndPoint)
   console.log(allUsers.data)
 
   const roseFirstStory = await axios
-    .post('http://localhost:3000/users/1', {
+    .post(`${userEndPoint}/1`, {
       hero: 'Zoe',
       title: 'Bed Time for Zeo',
       topic: 'Bed routine for 2 years old girl, she is trying to adapt with her bedtime routine',
