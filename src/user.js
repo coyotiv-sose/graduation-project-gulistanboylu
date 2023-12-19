835e4db8efe523b0b9fd0b88e43e03becfb4ea51
Gulistan Boylu <gulistan.boylu@gmail.com>
2023-12-19 20:44:37 +0000

    Refactor story list and story creation as they don not need to be static

--- src/user.js
+++ src/user.js
@@ -1,10 +1,19 @@
 const Story = require('./story')
 
 class User {
+  stories = []
   constructor(name, childAge, email) {
     this.name = name
     this.childAge = childAge
     this.email = email
+  }
+
+  storyCreate({ hero, title, topic, favoruitePeople, heroDescription }) {
+    const newStory = new Story(hero, title, topic, favoruitePeople, heroDescription)
+
+    this.stories.push(newStory)
+
+    return newStory
   }
 
   static list = []
@@ -13,18 +22,6 @@ class User {
 
     User.list.push(newUser)
     return newUser
-  }
-
-  static userStories = {}
-  static storyCreate({ email, hero, title, topic, favoruitePeople, heroDescription }) {
-    let storyList = []
-    const newStory = new Story(hero, title, topic, favoruitePeople, heroDescription)
-
-    storyList.push(newStory)
-    User.userStories[email] = storyList
-
-    console.log('story list', User.userStories)
-    return newStory
   }
 }
 
