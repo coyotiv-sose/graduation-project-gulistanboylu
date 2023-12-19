e84e5d4749875936ad7c1f6ec47f5fe0027b5f2e
Gulistan Boylu <gulistan.boylu@gmail.com>
2023-12-19 20:50:10 +0000

    Add the user as a parameter to post req to be able to use its functions

--- src/index.js
+++ src/index.js
@@ -15,17 +15,18 @@ async function createUser(name, childAge, email) {
     childAge,
     email,
   })
+  return user.data
 }
 
-async function createStory(email, hero, title, topic, favoruitePeople, heroDescription) {
+async function createStory(hero, title, topic, favoruitePeople, heroDescription, user) {
   const story = await axios
-    .post(`${userEndPoint}/1`, {
-      email,
+    .post(`${userEndPoint}/1/stories`, {
       hero,
       title,
       topic,
       favoruitePeople,
       heroDescription,
+      user,
     })
     .then(res => {
       console.log('response data ', res.data)
@@ -34,29 +35,30 @@ async function createStory(email, hero, title, topic, favoruitePeople, heroDescr
 
 //create a user
 async function main() {
-  createUser('Rose', 2, 'asd@gmail.com')
-  createUser('Buket', 1, 'hdede@gmail.com')
+  const rose = await createUser('Rose', 2, 'asd@gmail.com')
+  const buket = await createUser('Buket', 1, 'hdede@gmail.com')
+  console.log('rose u logluyoruz', rose)
 
-  const allUsers = await axios.get(userEndPoint)
-  console.log(allUsers.data)
-
-  createStory(
-    'asd@gmail.com',
+  await createStory(
     'Zoe',
     'Bed Time for Zeo',
     'Bed routine for 2 years old girl, she is trying to adapt with her bedtime routine',
     'Mum',
-    '2 years old girl with currly brown hair, lovely simile'
+    '2 years old girl with currly brown hair, lovely simile',
+    rose
   )
 
-  createStory(
-    'hdede@gmail.com',
+  await createStory(
     'Bea',
     'Bea gets a checkup',
     'Doctor checkoup for 2 year old and she will have her vaccination',
     'Mum, Dad',
-    '2 years old girl with strait blonde hair, lovely simile with a blue eyes'
+    '2 years old girl with strait blonde hair, lovely simile with a blue eyes',
+    buket
   )
+
+  const allUsers = await axios.get(userEndPoint)
+  console.log(allUsers.data[0].stories)
 }
 
 main()
