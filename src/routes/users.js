70b99019dd573667c472f45997ed2a9eb15bae06
Gulistan Boylu <gulistan.boylu@gmail.com>
2023-12-21 20:12:57 +0000

    Add route to get spesific user and render the view to list users

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,9 +3,19 @@ var router = express.Router()
 const User = require('../user')
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.status(200).send(User.list)
+  if (req.query.view === 'json') {
+    res.status(200).send(User.list)
+  }
+
+  res.render('users', { users: User.list })
 })
 
+// Get spesific user
+router.get('/:userEmail', function (req, res, next) {
+  const userEmail = req.params.userEmail
+  const user = User.findUserByEmail(userEmail)
+  res.status(200).send(user)
+})
 // Create a new user
 router.post('/', function (req, res, next) {
   const user = User.create({ name: req.body.name, childAge: req.body.childAge, email: req.body.email })
