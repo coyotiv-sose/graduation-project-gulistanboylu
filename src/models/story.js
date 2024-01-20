b7f43f0a9f6c997fcf40f77320ad5ce040fc53d5
Gulistan Boylu <gulistan.boylu@gmail.com>
2024-01-20 13:39:30 +0000

    Create schema for the story

--- src/models/story.js
+++ src/models/story.js
@@ -1,11 +1,22 @@
+const { default: mongoose } = require('mongoose')
+
 class Story {
-  constructor(hero, title, topic, favoruitePeople, heroDescription) {
-    this.hero = hero
-    this.title = title
-    this.topic = topic
-    this.favoruitePeople = favoruitePeople
-    this.heroDescription = heroDescription
-  }
+  // constructor(hero, title, topic, favoruitePeople, heroDescription) {
+  //   this.hero = hero
+  //   this.title = title
+  //   this.topic = topic
+  //   this.favoruitePeople = favoruitePeople
+  //   this.heroDescription = heroDescription
+  // }
 }
 
-module.exports = Story
+const storySchema = new mongoose.Schema({
+  hero: String,
+  title: String,
+  topic: String,
+  favoruitePeople: String,
+  heroDescription: String,
+})
+
+storySchema.loadClass(Story)
+module.exports = mongoose.model('Story', storySchema)
