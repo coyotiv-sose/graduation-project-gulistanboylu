ca774d35474876166e02c2c125d3d4bef12cfdd8
Gulistan Boylu <gulistan.boylu@gmail.com>
2024-01-21 21:46:49 +0000

    Get spesific story and also all stories from database

--- src/routes/stories.js
+++ src/routes/stories.js
@@ -3,16 +3,18 @@ var router = express.Router()
 const User = require('../models/user')
 const Story = require('../models/story')
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  // Todo : Check if email exist and if not return all stories as a response
-  const email = req.query.email
-  const userIndex = User.list.findIndex(user => user.email === email)
-  if (userIndex === -1) {
-    return res.status(404).send('Please provide an email for story list in the query')
-  }
-  const storiesForUser = User.list[userIndex].stories
-  res.status(200).send(storiesForUser)
+/* GET spesific story by id. */
+router.get('/:id', async function (req, res, next) {
+  const story = await Story.findById(req.params.id)
+  res.status(200).send(story)
+})
+
+/* GET all stories for the user. */
+router.get('/', async function (req, res, next) {
+  //todo: get user id from the session on the future.
+  const user = await User.findById(req.query.id)
+
+  res.status(200).send(user.stories)
 })
 
 router.post('/', async function (req, res, next) {
