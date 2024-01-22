abba8072b0162855f2f0967d3c051d3e1228bfbb
Gulistan Boylu <gulistan.boylu@gmail.com>
2024-01-22 07:21:48 +0000

    Create update story request and remove unused examples

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const axios = require('axios')
-// const User = require('./user')
 
-// const { response } = require('./app')
 console.log("Hi coyote, let's have some JavaScript fun! Yey!")
 
 //create user function
@@ -41,7 +39,7 @@ async function deleteUser(id) {
 async function main() {
   const rose = await createUser('Rose', 2, 'asd@gmail.com')
   const buket = await createUser('Buket', 1, 'hdede@gmail.com')
-  // console.log('rose log', rose)
+
   await createStory(
     'Zoe',
     'Bed Time for Zeo',
@@ -66,30 +64,7 @@ async function main() {
     '2 years old girl with strait blonde hair, lovely simile with a blue eyes',
     buket
   )
-  // const userStoryList = await axios.get(`${storiesEndPoint}?email=asd@gmail.com`).then(response => {
-  //   return response.data
-  // })
-  // const deleteStory = await axios
-  //   .delete(`${storiesEndPoint}/Bed Time for Zeo`, {
-  //     data: {
-  //       userEmail: 'asd@gmail.com',
-  //       storyTitle: 'Bed Time for Zeo', // or you can use story title: storyToDelete.title if you want to delete by title
-  //     },
-  //   })
-  //   .then(response => {
-  //     console.log('Deleted story: ', response.data)
-  //   })
-  // const updateStory = await axios
-  //   .put(`${storiesEndPoint}/Time to go: Pee`, {
-  //     userEmail: 'asd@gmail.com',
-  //     newStoryData: {
-  //       hero: 'Updated Hero with Numan',
-  //     },
-  //   })
-  //   .then(response => {
-  //     console.log('story updated', response.data)
-  //     return response.data
-  //   })
+
   const getRose = await axios.get(`${userEndPoint}/65ad40c81dd49bdee858155c`).then(response => {
     return response.data
   })
@@ -98,10 +73,6 @@ async function main() {
     console.log('all users', response.data)
     return response.data
   })
-
-  // console.log('all users', allUsers)
-  // console.log('storylist', userStoryList)
-  // console.log(updateStory)
 }
 
 async function testUSerDelete() {
@@ -120,6 +91,19 @@ async function testStoryDelete() {
     })
 }
 
-// main()
-testStoryDelete()
+async function testStoryUpdate() {
+  const updateStory = await axios
+    .put(`${storiesEndPoint}/65ad808b90939fcd3f26ee1e`, {
+      newStoryData: {
+        hero: 'Updated Hero with Numan',
+      },
+    })
+    .then(response => {
+      console.log('story updated')
+      return response.data
+    })
+}
+main()
+// testStoryDelete()
 // testUSerDelete()
+// testStoryUpdate()
