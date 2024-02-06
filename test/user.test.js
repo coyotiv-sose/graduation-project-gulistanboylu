e255b4c8885f8b6eca46e3ba79c15a85944c6895
Gulistan Boylu <gulistan.boylu@gmail.com>
2024-02-06 09:44:09 +0000

    Write new assertions for user

--- test/user.test.js
+++ test/user.test.js
@@ -4,5 +4,7 @@ require('../src/database-connection')
 
 test('Create a user is successful', async () => {
   const user = await User.create({ name: 'Gulistan', childAge: 2, email: 'gulistan.boylu@gmail.com' })
-  expect(user).toBeDefined()
+  expect(user.name).toBe('Gulistan')
+  expect(user.childAge).toBe(2)
+  expect(user.email).toBe('gulistan.boylu@gmail.com')
 })
